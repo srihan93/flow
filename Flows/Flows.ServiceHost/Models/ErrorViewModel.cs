eab3e83d715e3655b3ec18ee091ff6946f6d78a7
srihan93 <srihan93@gmail.com>
2021-05-24 22:57:52 +0530

    added projects and initial setup

---
+++ Flows/Flows.ServiceHost/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Flows.ServiceHost.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
