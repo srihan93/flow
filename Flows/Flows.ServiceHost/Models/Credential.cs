ffaadd7f449218e76973e7bcf549757716d80356
srihan93 <srihan93@gmail.com>
2021-05-26 00:13:09 +0530

    adding validation

--- Flows/Flows.ServiceHost/Models/Credential.cs
+++ Flows/Flows.ServiceHost/Models/Credential.cs
@@ -1,9 +1,13 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Flows.ServiceHost.Models
 {
     public class Credential
     {
+        [Required]
         public string EmailAddress { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
